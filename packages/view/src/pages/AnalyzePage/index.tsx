d0fd11b194a85f9e594958793f8715116e194808
Shamaralyy <2670602083@qq.com>
2023-08-15 13:49:23 +0800

    feat: internationalization

--- packages/view/src/pages/AnalyzePage/index.tsx
+++ packages/view/src/pages/AnalyzePage/index.tsx
@@ -1,11 +1,16 @@
 import { Tree } from "../../components/Tree";
 import { useStore } from "../../contexts";
 import Sidebar from "./Sidebar";
+import useLanguage from "../../i18n/hooks/useLanguage";
 
 export default function AnalyzePage() {
   const root = useStore((state) => state.root);
+  const { t, toggleLanguage } = useLanguage();
+
   return (
     <div className="h-screen overflow-hidden">
+      <button onClick={toggleLanguage}>切换语言</button>
+      <div>{t("section.depth", { num: 1 })}</div>
       <Tree originalData={root}></Tree>
       <Sidebar />
     </div>
