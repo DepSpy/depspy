dc3ecf68b75326ad341478983077cc42b07ac0a4
RunningLiLi <93420481+RunningLiLi@users.noreply.github.com>
2023-08-17 23:22:46 +0800

    Revert "fix: global dev script (#25)" (#26)
    This reverts commit 07315c173400c687457c710e9f71021acc3f08e1.

--- scripts/index.mjs
+++ scripts/index.mjs
@@ -1,5 +1,4 @@
 import { exec } from "child_process";
-import chokidar from "chokidar";
 import path from "path";
 import fs from "fs";
 
@@ -8,27 +7,9 @@ const root = process.cwd();
 const config = JSON.parse(
   fs.readFileSync(path.resolve(root, "depspy.script.json"), "utf8"),
 );
-const commands = Object.entries(config);
+const env = process.argv[2];
+const commands = Object.entries(config[env]);
 
-const watcher = chokidar.watch(path.resolve(root, "packages"), {
-  ignored: ["**/node_modules/**", "**/dist/**"],
-});
-watcher.addListener("change", (changePath) => {
-  commands.forEach(([packageName, commands]) => {
-    const packagePath = path.resolve(root, "packages", packageName);
-    if (changePath.includes(packagePath)) {
-      commands.forEach((command) => {
-        exec(command, { cwd: packagePath }, (err, stdout) => {
-          if (err) {
-            console.error(`${packageName} failed:`, err);
-          } else {
-            console.log(`${packageName} succeeded:`, stdout);
-          }
-        });
-      });
-    }
-  });
-});
 commands.forEach(([packageName, commands]) => {
   const packagePath = path.resolve(root, "packages", packageName);
   commands.forEach((command) => {
