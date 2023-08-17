d3a37abccfa491f8b6aa08b797660d6b2364bd1f
RunningLiLi <93420481+RunningLiLi@users.noreply.github.com>
2023-08-18 00:28:23 +0800

    fix: sync script

--- scripts/index.mjs
+++ scripts/index.mjs
@@ -1,8 +1,10 @@
-import { exec } from "child_process";
+import { execSync, exec } from "child_process";
 import chokidar from "chokidar";
 import path from "path";
 import fs from "fs";
-
+import chalk from "chalk";
+const { green, yellow } = chalk;
+console.log(yellow("开始构建..."));
 const root = process.cwd();
 
 const config = JSON.parse(
@@ -23,6 +25,7 @@ watcher.addListener("change", (changePath) => {
             console.error(`${packageName} failed:`, err);
           } else {
             console.log(`${packageName} succeeded:`, stdout);
+            console.log(green(`${packageName}包变动，已重新构建`));
           }
         });
       });
@@ -32,12 +35,7 @@ watcher.addListener("change", (changePath) => {
 commands.forEach(([packageName, commands]) => {
   const packagePath = path.resolve(root, "packages", packageName);
   commands.forEach((command) => {
-    exec(command, { cwd: packagePath }, (err, stdout) => {
-      if (err) {
-        console.error(`${packageName} failed:`, err);
-      } else {
-        console.log(`${packageName} succeeded:`, stdout);
-      }
-    });
+    execSync(command, { cwd: packagePath });
   });
 });
+console.log(green("构建成功，监听中..."));
