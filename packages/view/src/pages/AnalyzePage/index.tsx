356486aa04fdf0d6e47621516f472d657e8e3fa9
Shamaralyy <2670602083@qq.com>
2023-08-18 16:35:16 +0800

    feat: skin switching

--- packages/view/src/pages/AnalyzePage/index.tsx
+++ packages/view/src/pages/AnalyzePage/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Tree } from "../../components/Tree";
 import { useStore } from "../../contexts";
 import Sidebar from "./Sidebar";
@@ -6,10 +7,20 @@ import useLanguage from "../../i18n/hooks/useLanguage";
 export default function AnalyzePage() {
   const root = useStore((state) => state.root);
   const { t, toggleLanguage } = useLanguage();
+  const [mode, setMode] = useState<string>("light");
+
+  const toggleMode = () => {
+    const newMode = mode === "light" ? "dark" : "light";
+    setMode(newMode);
+  };
 
   return (
     <div className="h-screen overflow-hidden">
       <button onClick={toggleLanguage}>切换语言</button>
+      <button onClick={toggleMode}>切换模式</button>
+      <p className={`bg-${mode}-bg c-${mode}-text`}>
+        This is primary text-{mode}
+      </p>
       <div>{t("section.depth")}</div>
       <Tree originalData={root}></Tree>
       <Sidebar />
--- packages/view/uno.config.ts
+++ packages/view/uno.config.ts
@@ -1,5 +1,48 @@
-import { defineConfig } from "unocss";
+import {
+  defineConfig,
+  presetAttributify,
+  presetUno,
+  presetIcons,
+} from "unocss";
 
 export default defineConfig({
+  presets: [
+    presetUno(),
+    presetAttributify(),
+    presetIcons({ scale: 1.2, warn: true }),
+  ],
+  shortcuts: [],
   rules: [],
+  theme: {
+    colors: {
+      light: {
+        bg: "#E6E2F2",
+        bgHover: "#CABEF3",
+        border: "#A992F6",
+        borderHover: "#8F71F3",
+        hover: "#7550F1",
+        base: "#5B2EEE",
+        active: "#3A0FC6",
+        textHover: "#260A84",
+        text: "#1D0763",
+        textActive: "#130542",
+        icon: "#00000073",
+        iconHover: "#000000e0",
+      },
+      dark: {
+        bg: "#000000",
+        bgHover: "#CABEF3",
+        border: "#00000026",
+        borderHover: "#8F71F3",
+        hover: "#7550F1",
+        base: "#5B2EEE",
+        active: "#3A0FC6",
+        textHover: "#260A84",
+        text: "#fff",
+        textActive: "#130542",
+        icon: "#00000073",
+        iconHover: "#000000e0",
+      },
+    },
+  },
 });
