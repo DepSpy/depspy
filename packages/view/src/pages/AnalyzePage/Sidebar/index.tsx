0cd294eee52d0bd398fca45f721ea33f5df7c5d6
CheerioInf <lltxsbsbg@gmail.com>
2023-08-20 23:59:35 +0800

    feat: add treelist style and i18n

--- packages/view/src/pages/AnalyzePage/Sidebar/index.tsx
+++ packages/view/src/pages/AnalyzePage/Sidebar/index.tsx
@@ -4,13 +4,17 @@ import ChooseItem from "./ChooseItem";
 import SideSearch from "../../../components/SideSearch";
 import SideModule from "../../../components/SideModule";
 import "./index.scss";
+import useLanguage from "../../../i18n/hooks/useLanguage";
 
 export default function Side() {
+  const { t } = useLanguage();
   const typeList = {
-    SEARCH: <SideSearch></SideSearch>,
-    GRAPH: <SideTreeList />,
-    MODULE: <SideModule></SideModule>,
+    [t("aside.search.title")]: <SideSearch></SideSearch>,
+    [t("aside.list.title")]: <SideTreeList />,
+    [t("aside.module.title")]: <SideModule></SideModule>,
   };
+
+  // 获取 typeList 的属性名
   const typeName = Object.keys(typeList);
 
   const [showName, setShowName] = useState(typeName[1]);
