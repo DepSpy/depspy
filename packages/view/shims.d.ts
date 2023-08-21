da5c9dfbaf1627a51855b8dd62241564245fffe8
RunningLiLi <93420481+RunningLiLi@users.noreply.github.com>
2023-08-21 20:10:43 +0800

    feat: depth&collapse (#44)

---
+++ packages/view/shims.d.ts
@@ -0,0 +1,6 @@
+import type { AttributifyAttributes } from "@unocss/preset-attributify";
+
+declare module "react" {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  interface HTMLAttributes<T> extends AttributifyAttributes {}
+}
