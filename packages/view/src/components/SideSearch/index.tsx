6a9eda418a961a1dc1baeb41be771b7b96661322
lingkongsiji <2424460304@qq.com>
2023-08-24 18:11:39 +0800

    feat: search history

--- packages/view/src/components/SideSearch/index.tsx
+++ packages/view/src/components/SideSearch/index.tsx
@@ -22,23 +22,31 @@ export default function SideSearch() {
     keywords: "",
     loading: true,
     nodes: [],
+    searchHistory: JSON.parse(localStorage.getItem("suggestionHistory")) ?? [],
   });
   const searchHandler = (node: Node) => {
     if (node !== null) setSelectNode(node);
     dispatch([
       { type: "keywords", value: "" },
       { type: "nodes", value: [] },
+      {
+        type: "searchHistory",
+        value: [...new Set([...state.searchHistory, node.name])],
+      },
     ]);
   };
   const searchResults = useMemo(
     () => (
-      <ul>
-        {state.nodes.map((v, i) => (
-          <li key={i} onClick={() => searchHandler(v)}>
-            {v.name}
-          </li>
-        ))}
-      </ul>
+      <>
+        <h1>{t("aside.search.results")}</h1>
+        <ul>
+          {state.nodes.map((v, i) => (
+            <li key={i} onClick={() => searchHandler(v)}>
+              {v.name}
+            </li>
+          ))}
+        </ul>
+      </>
     ),
     [state.nodes],
   );
@@ -50,6 +58,16 @@ export default function SideSearch() {
       dispatch({ type: "nodes", value: [] });
     } else dispatch({ type: "nodes", value: false });
   }, [state.keywords]);
+  useEffect(() => {
+    if (!state.loading)
+      localStorage.setItem(
+        "suggestionHistory",
+        JSON.stringify(state.searchHistory),
+      );
+  }, [state.searchHistory]);
+  useEffect(() => {
+    dispatch({ type: "loading", value: false });
+  }, []);
   return (
     <div className={styles["side-search"]}>
       <div className={styles["find"]}>
@@ -68,7 +86,6 @@ export default function SideSearch() {
         </div>
       </div>
       <div className={styles["result"]}>
-        <h1>{t("aside.search.results")}</h1>
         {!!state.nodes.length && searchResults}
       </div>
     </div>
