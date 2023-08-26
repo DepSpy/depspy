3b8834418af28a317b938dc507a0c549bc8b7df3
CheerioInf <lltxsbsbg@gmail.com>
2023-08-26 14:11:15 +0800

    feat: add back to preNode button

--- packages/view/src/pages/AnalyzePage/index.tsx
+++ packages/view/src/pages/AnalyzePage/index.tsx
@@ -10,7 +10,7 @@ import { shallow } from "zustand/shallow";
 import { LanguageIcon, ThemeIcon } from "../../components/icon/index";
 export default function AnalyzePage() {
   const [searchParams] = useSearchParams();
-  const { root, info, depth, setGraphRes } = useStore(
+  const { root, info, depth, setGraphRes, setPreSelectNode } = useStore(
     (state) => state,
     shallow,
   );
@@ -33,6 +33,17 @@ export default function AnalyzePage() {
       <div className="flex h-screen overflow-hidden bg-bg-container">
         <Tree ref={svg}></Tree>
         <Sidebar />
+        <div
+          className="absolute flex right-105 top-5
+           z-10 p-2 w-15 h-15
+           bg-bg-container
+           border border-solid border-border rounded-full hover:border-primary-base"
+          onClick={() => {
+            setPreSelectNode();
+          }}
+        >
+          <div className="i-carbon-direction-loop-left w-full h-full text-icon"></div>
+        </div>
       </div>
       <section
         className="fixed flex left-2rem bottom-2rem gap-4 h-2rem"
