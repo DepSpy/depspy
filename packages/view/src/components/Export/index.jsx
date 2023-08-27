8b4c7d93c7b7cecce18495617ae611bffb722b11
RunningLiLi <93420481+RunningLiLi@users.noreply.github.com>
2023-08-27 18:48:27 +0800

    fix: exprot png (#74)

--- packages/view/src/components/Export/index.jsx
+++ packages/view/src/components/Export/index.jsx
@@ -1,5 +1,6 @@
 import * as d3 from "d3";
 import { useRef } from "react";
+import { useStore } from "@/contexts";
 import useLanguage from "@/i18n/hooks/useLanguage";
 import saveSvg from "save-svg-as-png";
 
@@ -8,23 +9,31 @@ export function Export({ svgRef, width, height, json }) {
   let { current } = useRef({
     fileTypeRef: "",
   });
+  const [depth] = useStore((state) => [state.depth, state.setDepth]);
+  const { collapse } = useStore((state) => ({
+    collapse: state.collapse,
+    setCollapse: state.setCollapse,
+  }));
   const handleSelectChange = () => {
     const select = document.getElementsByTagName("select")[0];
     const selectedType = select.options[select.selectedIndex].value;
     current.fileTypeRef = selectedType;
   };
   // 缩放获取完整svg d3 矢量
-  const zoomScreen = (type) => {
+  const zoomScreen = (type, sb = {}) => {
     function zoomed(e) {
       d3.selectAll("#resizing").attr("transform", e.transform);
     }
-    const zoom = d3.zoom().scaleExtent([1, 0.1]).on("zoom", zoomed);
-    if (type == "Full")
+    const zoom = d3.zoom().on("zoom", zoomed);
+    if (type == "Full") {
+      const divide = collapse ? 1 : depth < 3 ? 1 : Math.pow(2.1, depth);
       d3.select(svgRef.current).call(
         zoom.transform,
-        d3.zoomIdentity.translate(width / 100, height / 2).scale(0.1),
+        d3.zoomIdentity
+          .translate(width / sb.width, height / (100 / divide))
+          .scale(0.01),
       );
-    else if (type == "Reset")
+    } else if (type == "Reset")
       d3.select(svgRef.current).call(zoom.transform, d3.zoomIdentity);
   };
   const options = [
@@ -55,17 +64,19 @@ export function Export({ svgRef, width, height, json }) {
   }
 
   function onSaveSvg() {
-    zoomScreen("Full");
     const svgDom = document.querySelector("svg");
-    const serializer = new XMLSerializer();
+    const sb = svgDom.getBBox();
+    zoomScreen("Full", sb);
     const toExport = svgDom.cloneNode(true);
     const bb = svgDom.getBBox();
+    console.log(sb, bb);
+    const serializer = new XMLSerializer();
     toExport.setAttribute(
       "viewBox",
       bb.x + " " + bb.y + " " + bb.width + " " + bb.height,
     );
-    toExport.setAttribute("width", bb.width * 20);
-    toExport.setAttribute("height", bb.height * 20);
+    toExport.setAttribute("width", bb.width * 250);
+    toExport.setAttribute("height", bb.height * 250);
     const source =
       '<?xml version="1.0" standalone="no"?>\r\n' +
       serializer.serializeToString(toExport);
@@ -77,20 +88,21 @@ export function Export({ svgRef, width, height, json }) {
     zoomScreen("Reset");
   }
   const onSaveImage = (format) => {
-    zoomScreen("Full");
     const svgDom = document.querySelector("svg");
+    const sb = svgDom.getBBox();
+    zoomScreen("Full", sb);
     const toExport = svgDom.cloneNode(true);
     const bb = svgDom.getBBox();
-    toExport.setAttribute(
-      "viewBox",
-      `${bb.x}" "${-bb.y}" "${bb.width}" "${bb.height}`,
-    );
-    toExport.setAttribute("width", bb.width * 1.2);
-    toExport.setAttribute("height", bb.height * 1.2);
+    const view = `${bb.x * 50} ${bb.y * 50} ${bb.width * 1.2} ${
+      bb.height * 1.3
+    }`;
+    toExport.setAttribute("viewBox", view);
+    toExport.setAttribute("width", bb.width * 250);
+    toExport.setAttribute("height", bb.height * 250);
     saveSvg.saveSvgAsPng(
       toExport,
       `${json.name}${t("section.dependences")}.${format}`,
-      { scale: 10 },
+      { scale: 80 },
     );
     zoomScreen("Reset");
   };
