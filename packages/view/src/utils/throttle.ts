9b98a358130deb218437a17fc50655155db5e127
CheerioInf <109421039+cheerioInf@users.noreply.github.com>
2023-08-28 21:58:53 +0800

    feat: improve tree back (#86)
    * feat: add github icon and homepage toast
    
    * fix: online mode cant load size module
    
    * feat: add size module loading and reset
    
    * feat: add background
    
    * fix: background adapt
    
    * feat: add screen self-adaptation

---
+++ packages/view/src/utils/throttle.ts
@@ -0,0 +1,10 @@
+export const throttle = (func, delay = 500) => {
+  let timer = null;
+  return (...args) => {
+    if (timer) return;
+    timer = setTimeout(() => {
+      func(...args);
+      timer = null;
+    }, delay);
+  };
+};
