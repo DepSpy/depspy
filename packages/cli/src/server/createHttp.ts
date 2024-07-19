678986fe2251d7c6d96b000b2af1d71381355345
“tanghuayang” <tanghuayang@xiaohongshu.com>
2024-07-19 12:10:00 +0800

    feat: modify the response type

--- packages/cli/src/server/createHttp.ts
+++ packages/cli/src/server/createHttp.ts
@@ -1,17 +1,11 @@
 import { Express } from "express";
 import { Graph, Node } from "@dep-spy/core";
 
-interface Response<T> {
-  code: number;
-  data: T;
-  msg: string;
-}
-
 export function createHttp(app: Express, graph: Graph) {
-  app.get<{ id?: string; depth: number }, Response<Node>>(
+  app.get<{ id?: string; depth: number }, string>(
     "/getNode",
     async (req, res) => {
-      const id = req.query.id;
+      const id = String(req.query.id);
       const depth = Number(req.query.depth);
       res.send(graph.getNode(id, depth));
     },
