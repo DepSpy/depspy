2ea0b1d7a641e23c7eba296fa76dd52767228656
estarhp <tanghuayanglex@gmail.com>
2024-11-28 18:03:26 +0800

    feat: fix circular reference

--- packages/core/src/dep/onlineModuleInfoPool.ts
+++ packages/core/src/dep/onlineModuleInfoPool.ts
@@ -1,8 +1,9 @@
 import { HOST_MAX_FETCH_NUMBER, NPM_DOMAINS } from "../constant";
 import { FunctionPool, getModuleInfo, FunctionWorker } from "@dep-spy/utils";
+const inBrowser = typeof window !== "undefined";
 
 export const onlineModuleInfoPool = new FunctionPool(
-  NPM_DOMAINS.length * HOST_MAX_FETCH_NUMBER,
+  inBrowser ? NPM_DOMAINS.length * HOST_MAX_FETCH_NUMBER : 0,
   (index: number) => {
     const url = NPM_DOMAINS[Math.floor(index % NPM_DOMAINS.length)];
     function getModuleInfoByDomains({
--- packages/core/src/threadsPool/index.ts
+++ packages/core/src/threadsPool/index.ts
@@ -1,26 +1,7 @@
-import { getModuleInfo, ThreadsPool, ThreadsWorker } from "@dep-spy/utils";
+import { ThreadsPool, ThreadsWorker } from "@dep-spy/utils";
 import os from "os";
 import path from "path";
-
-export const enum TASK_TYPE {
-  MODULE_INFO = "module_info",
-  MESSAGE = "message",
-}
-
-// 处理函数参数必须为配置对象
-export const EventBus = {
-  [TASK_TYPE.MODULE_INFO]: async (options: {
-    info: string;
-    baseDir: string;
-  }) => {
-    return await getModuleInfo(options);
-  },
-
-  //测试示例
-  [TASK_TYPE.MESSAGE]: async (options: { name: number }) => {
-    console.log(options.name);
-  },
-};
+import { EventBus, TASK_TYPE } from "./eventBus";
 
 // 本地线程池
 export default new ThreadsPool<typeof EventBus>(
@@ -32,3 +13,5 @@ export default new ThreadsPool<typeof EventBus>(
     });
   },
 );
+
+export { EventBus, TASK_TYPE };
